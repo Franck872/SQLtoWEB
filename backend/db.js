becc7bc948b5681d02ba4de3cb2d00b0d691cf2d
Franck872 <reytrouv@gmail.com>
2026-03-10 17:12:00 +0000

    Create db.js

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pkg;
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  max: 10
+});
+
+pool.on("connect", () => {
+  console.log("✅ Connected to PostgreSQL");
+});
